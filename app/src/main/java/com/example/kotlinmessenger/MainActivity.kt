f5a4f68a1efd73967ddf7dc1b9f2bff072e20a90
OndrejKulhavy <okulhav@gmail.com>
2019-08-06 16:15:44 +0200

    Zacatek

---
+++ app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.kotlinmessenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        register_button.setOnClickListener(){
+       val username = username_edittext.text.toString()
+       val email = email_edittext.text.toString()
+       val password = password_edittext.text.toString()
+            Log.d("MainActivity", "tvuj emaile je: " + email)
+            Log.d("MainActivity", "tvoje heslo je: " + password)
+            Log.d("MainActivity", "tvuj uzivatelske jmeno je: " + username)
+            Log.d("MainActivity", "---------------------------------------------------")
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
