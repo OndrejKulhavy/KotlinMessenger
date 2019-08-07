82fdeaf16a2126042eafa01ed283f72ddc5a1f09
OndrejKulhavy <okulhav@gmail.com>
2019-08-07 21:22:19 +0200

    Uploading profile picture

--- app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity(){
@@ -12,6 +13,14 @@ class LoginActivity: AppCompatActivity(){
         setContentView(R.layout.activity_login)
 
 
+        log_in_button.setOnClickListener(){
+        val  email = email_login_edittext.text.toString()
+        val  password = password_login_edittext.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            //    .addOnCompleteListener()
+            // .add
+        }
 
         i_have_an_account.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
--- app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.kotlinmessenger
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.util.*
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
@@ -15,28 +23,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button.setOnClickListener(){
-       val username = username_edittext.text.toString()
-       val email = email_edittext.text.toString()
-       val password = password_edittext.text.toString()
-            Log.d("MainActivity", "tvuj emaile je: " + email)
-            Log.d("MainActivity", "tvoje heslo je: " + password)
-            Log.d("MainActivity", "tvuj uzivatelske jmeno je: " + username)
-            Log.d("MainActivity", "----------------------------------------------------")
-
-
-                    //ověření firebase
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener{
-                    if (!it.isSuccessful) return@addOnCompleteListener
-
-                    Log.d("Main", "Succesfully created user")
-
-                }
-
-                if (email.isEmpty() || password.isEmpty()) return@setOnClickListener
-
-
+            performRegister()
         }
+
         Already_have_an_acount_edittext.setOnClickListener(){
             Log.d("MainActivity", "Funguje nacitani loginu")
             val intent = Intent(this, LoginActivity::class.java)
@@ -44,16 +33,57 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-
-
-
-
-
-
-
-
+        select_photo_button.setOnClickListener {
+        val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
 
 
 
     }
+    var selectedPhotoUri :   Uri? = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0 && resultCode ==Activity.RESULT_OK && data != null){
+             selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+            val bitmapDrawable =BitmapDrawable(bitmap)
+            select_photo_button.setBackgroundDrawable(bitmapDrawable)
+        }
+
+    }
+
+private fun  performRegister(){
+    val username = username_edittext.text.toString()
+    val email = email_edittext.text.toString()
+    val password = password_edittext.text.toString()
+    if (email.isEmpty() || password.isEmpty()){
+        Toast.makeText(this, "Pro pokračování musíš vyplnit email a heslo", Toast.LENGTH_SHORT).show()
+        return
+      }
+    //ověření firebase
+    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+        .addOnCompleteListener{
+            if (!it.isSuccessful) return@addOnCompleteListener
+            Log.d("Main", "Succesfully created user")
+
+            uploadImageToFirebaseStorage()
+            }
+        .addOnFailureListener{
+            Toast.makeText(this, "Chyba při vytváření účtu: ${it.message}", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private fun uploadImageToFirebaseStorage(){
+        if (selectedPhotoUri == null ) return
+    val  filename = UUID.randomUUID().toString()
+   val  ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+        ref.putFile(selectedPhotoUri !!)
+            .addOnSuccessListener {
+                Log.d("Register", "Uspesne nahrany obrazek ${it.metadata?.path}")
+            }
+    }
+
 }
