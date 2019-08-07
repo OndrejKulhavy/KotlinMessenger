d2986a7b39636b29c3636d2e9492c7f0d0e44a54
OndrejKulhavy <okulhav@gmail.com>
2019-08-07 17:08:44 +0200

    Implementace Google firebase

--- app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/LoginActivity.kt
@@ -1,12 +1,37 @@
 package com.example.kotlinmessenger
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+        i_have_an_account.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.log
 
@@ -21,6 +22,20 @@ class MainActivity : AppCompatActivity() {
             Log.d("MainActivity", "tvoje heslo je: " + password)
             Log.d("MainActivity", "tvuj uzivatelske jmeno je: " + username)
             Log.d("MainActivity", "----------------------------------------------------")
+
+
+                    //ověření firebase
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener{
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    Log.d("Main", "Succesfully created user")
+
+                }
+
+                if (email.isEmpty() || password.isEmpty()) return@setOnClickListener
+
+
         }
         Already_have_an_acount_edittext.setOnClickListener(){
             Log.d("MainActivity", "Funguje nacitani loginu")
